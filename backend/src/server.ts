ab3d2791bd4a54e7834e22abc2ab833bf771bb06
Ebere <eiweala@live.com>
2023-12-10 11:58:43 -0800

    initial upload

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -38,25 +38,14 @@ server.post("/api/v1/images", async (req: ImageRequest, res: Response) => {
         message: "Company name or company sector is invalid",
       });
     }
-    // const result = await openaiClient.images.generate({
-    //   model: "dall-e-2",
-    //   prompt: `create a vector logo for ${req.body.companyName} sector called ${req.body.companySector}`,
-    //   size: "256x256",
-    //   n: 3,
-    // });
-    //
-    // return res.status(StatusCodes.OK).json(result.data);
-    return res.status(StatusCodes.OK).json([
-      {
-        url: "https://oaidalleapiprodscus.blob.core.windows.net/private/org-GLF5p9CsJvpjXWIJGU1PS3Se/user-jNr65bMagT2QyzBJGXrQnkVx/img-O3eoL2CPfxUjcUbT9ncBwQFV.png?st=2023-12-10T16%3A10%3A36Z&se=2023-12-10T18%3A10%3A36Z&sp=r&sv=2021-08-06&sr=b&rscd=inline&rsct=image/png&skoid=6aaadede-4fb3-4698-a8f6-684d7786b067&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2023-12-09T23%3A12%3A48Z&ske=2023-12-10T23%3A12%3A48Z&sks=b&skv=2021-08-06&sig=wL/L%2BJCTbeiBhYWbVgjmFhQijpSL/BNOrH3IzDn8n04%3D",
-      },
-      {
-        url: "https://oaidalleapiprodscus.blob.core.windows.net/private/org-GLF5p9CsJvpjXWIJGU1PS3Se/user-jNr65bMagT2QyzBJGXrQnkVx/img-CZqO8djBJFeWIlyLI7kgOLNF.png?st=2023-12-10T16%3A10%3A36Z&se=2023-12-10T18%3A10%3A36Z&sp=r&sv=2021-08-06&sr=b&rscd=inline&rsct=image/png&skoid=6aaadede-4fb3-4698-a8f6-684d7786b067&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2023-12-09T23%3A12%3A48Z&ske=2023-12-10T23%3A12%3A48Z&sks=b&skv=2021-08-06&sig=7zXtX2rxzw2owXAeCiK97TvF7/9JWEqXhVvs3vmFJZ4%3D",
-      },
-      {
-        url: "https://oaidalleapiprodscus.blob.core.windows.net/private/org-GLF5p9CsJvpjXWIJGU1PS3Se/user-jNr65bMagT2QyzBJGXrQnkVx/img-xViEhH5Vv6UHQAefcdiNJHWn.png?st=2023-12-10T16%3A10%3A36Z&se=2023-12-10T18%3A10%3A36Z&sp=r&sv=2021-08-06&sr=b&rscd=inline&rsct=image/png&skoid=6aaadede-4fb3-4698-a8f6-684d7786b067&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2023-12-09T23%3A12%3A48Z&ske=2023-12-10T23%3A12%3A48Z&sks=b&skv=2021-08-06&sig=Pv0RJ6NlyBXuG/GtOWkkrfQPMhI5sl5O6/k1vv2na6o%3D",
-      },
-    ]);
+    const result = await openaiClient.images.generate({
+      model: "dall-e-2",
+      prompt: `create a vector logo for ${req.body.companyName} sector called ${req.body.companySector}`,
+      size: "256x256",
+      n: 3,
+    });
+
+    return res.status(StatusCodes.OK).json(result.data);
   } catch (e) {
     console.log(e);
     res.status(StatusCodes.SERVICE_UNAVAILABLE).json({});
